d8ccdad94b511e61c9e7d49036c539714b9dae3d
rezuanhossain <rezwan35-2219@diu.edu.bd>
2020-07-21 02:37:04 +0600

    Show photos

--- swegallery/gallery/views.py
+++ swegallery/gallery/views.py
@@ -1,7 +1,17 @@
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from gallery.models import Gallery
+
 
 def home(request):
-    return HttpResponse('zero')
+    gallery_list = Gallery.objects.all()
+    paginator = Paginator(gallery_list,3)
+    page = request.GET.get('page')
+    page_listings = paginator.get_page(page)
+    context = {
+        'gallery_list':page_listings
+    }
+    return render(request,'gallery/photos.html',context)
